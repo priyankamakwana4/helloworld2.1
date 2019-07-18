731bae3a3777f52fc87fc8a97406ff8014cb9370
priyankamakwana4 <40364607+priyankamakwana4@users.noreply.github.com>
2019-07-18 13:31:30 -0700

    Add files via upload

---
+++ HelloWorld2.1/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld2._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hello World!" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            return "Hi " + id + "!";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ContentResult Post([FromBody] string value)
+        {
+            var contentResult = new ContentResult
+            {
+                Content = "How are you, " + value,
+                StatusCode = StatusCodes.Status200OK
+            };
+            return contentResult;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
